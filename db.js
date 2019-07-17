490e4d8d0189cdbf26f5228c5c0baad002cd6a5f
Dean Sumitro <DSumitro@talentpath.com>
2019-07-17 11:25:04 -0700

    init commit

--- db.js
+++ db.js
@@ -15,13 +15,62 @@ ex after running delete('user.json'):
 
 Errors should also be logged (preferably in a human-readable format)
 */
+/**
+ * logs all requests in log.txt
+ * @param {string} value
+ */
+function log(action, file = '', value = '') {
+  return fs.appendFile(
+    'log.txt',
+    `${action} ${value} in ${file} at ${Date.now()}\n`
+  );
+}
 
 /**
- * Logs the value of object[key]
+ * Logs out the value of object[key]
  * @param {string} file
  * @param {string} key
  */
-function get(file, key) {}
+// function get (file, key){
+// readfile
+// fs.readfile(`./${file}`)
+// handle promise and get back data
+// parse data from string into JSON
+// use key to get value
+// append the log file with the above value
+// }
+// using promises
+function get(file, key) {
+  return fs
+    .readFile(file, 'utf-8')
+    .then(data => {
+      const parsed = JSON.parse(data);
+      // bracket notation because if using dot notation.
+      // it looks for the key called "key"
+      const value = parsed[key];
+      if (!value) return log(`ERROR no "${key}" key on ${file}`);
+      return log('got', file, value);
+    })
+    .catch(err => log(`ERROR no such file or directory ${file}`));
+}
+// using async
+// is underlined because apparently not used?
+async function getAsync(file, key) {
+  try {
+    // 1. read file
+    // 2. handle data
+    const data = await fs.readFile(file, 'utf8');
+    // 3. parse data from string --> JSON
+    const parsed = JSON.parse(data);
+    // 4. use the key to get the value at object[key]
+    const value = parsed[key];
+    if (!value) return log(`ERROR no "${key}" key on ${file}`);
+    // 5. append the log file with the value
+    return log('got', file, value);
+  } catch (err) {
+    return log(`ERROR no such file or directory ${file}`);
+  }
+}
 
 /**
  * Sets the value of object[key] and rewrites object to file
@@ -29,28 +78,78 @@ function get(file, key) {}
  * @param {string} key
  * @param {string} value
  */
-function set(file, key, value) {}
+function set(file, key, value) {
+  return (
+    fs
+      // reads
+      .readFile(file, 'utf8')
+      .then(data => {
+        // parse
+        const parsed = JSON.parse(data);
+        // adds property the converts back to JSON format
+        parsed[key] = value;
+        const newObj = JSON.stringify(parsed);
+        return fs.writeFile(file, newObj);
+      })
+      // TODO: adjust log function
+      .then(() => log(`${key}: ${value} set in ${file}`))
+      .catch(() => log(`ERROR no such file or directory ${file}`))
+  );
+}
 
 /**
  * Deletes key from object and rewrites object to file
  * @param {string} file
  * @param {string} key
  */
-function remove(file, key) {}
+// ESLint has a convert to async function?
+function remove(file, key) {
+  return fs
+    .readFile(file, 'utf8')
+    .then(data => {
+      const parsed = JSON.parse(data);
+      // not sure if needed error handle
+      if (!parsed[key]) return log(`ERROR no "${key}" key on ${file}`);
+      delete parsed[key];
+      const newObj = JSON.stringify(parsed);
+      fs.writeFile(file, newObj);
+      return log('removed', file, key);
+    })
+    .catch(err => log(`ERROR no such file or directory ${file}`));
+}
 
 /**
  * Deletes file.
  * Gracefully errors if the file does not exist.
  * @param {string} file
  */
-function deleteFile(file) {}
-
+function deleteFile(file) {
+  return (
+    fs
+      // TODO: Figure out why it will not log
+      // Most likely asynchonicity?
+      // take return log out and but it in a then?
+      // maybe fixed
+      .unlink(file)
+      .then(() => {
+        log(`${file} was delete`);
+      })
+      .catch(() => log('file does not exist'))
+  );
+}
 /**
  * Creates file with an empty object inside.
  * Gracefully errors if the file already exists.
  * @param {string} file JSON filename
  */
-function createFile(file) {}
+function createFile(file) {
+  return fs
+    .access(`${file}`)
+    .then(() => log(`Cannot create file, ${file}, already exists`))
+    .catch(() => fs.writeFile(`${file}`, '{}')
+    .then(() => log('Succefully created ${file}'));
+  // return fs.writeFile(file, JSON.stringify({}));
+}
 
 /**
  * Merges all data into a mega object and logs it.
@@ -70,7 +169,9 @@ function createFile(file) {}
  *    }
  * }
  */
-function mergeData() {}
+function mergeData() {
+  
+}
 
 /**
  * Takes two files and logs all the properties as a list without duplicates
@@ -104,9 +205,11 @@ function difference(fileA, fileB) {}
 
 module.exports = {
   get,
+  getAsync,
   set,
   remove,
   deleteFile,
+  deleteFileAsync,
   createFile,
   mergeData,
   union,
